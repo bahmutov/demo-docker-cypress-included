10a704d2429afe77b3bb421bf689b785dad06245
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-01 22:21:16 -0400

    pass arguments

--- cy-run.sh
+++ cy-run.sh
@@ -1,9 +1,13 @@
-set e+x
+echo "Running Cypress e2e tests headlessly without copying files"
 
-echo "Running Cypress e2e tests headlessly"
-
-# run Cypress headlessly inside a Docker container
-#  -it = interactive terminal
+# explanation of the "docker run" command line arguments
+#
+#  -it          = interactive terminal
 #  -v $PWD:/e2e = map current folder to /e2e inside the container
-#  -w /e2e = set working directy to /e2e
-docker run -it -v $PWD:/e2e -w /e2e cypress/included:3.2.0
+#  -w /e2e      = set working directy to /e2e
+#  $@           = pass any arguments to this script to the Cypress command
+#                 like "./cy-run.sh --record"
+#
+# Docker image "cypress/included:3.2.0" has its entrypoint
+# set to "cypress run" by default
+docker run -it -v $PWD:/e2e -w /e2e cypress/included:3.2.0 $@
