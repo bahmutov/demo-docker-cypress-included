5c46c0894202f474b2ec02e720d68f77883adc5d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-01 23:08:44 -0400

    add image screenshot

--- README.md
+++ README.md
@@ -30,4 +30,23 @@ Running Cypress e2e tests headlessly
 
   Cypress TodoMVC test
     ✓ adds 2 todos (1351ms)
+
+  ...
 ```
+
+## See help
+
+- specify entrypoint to be just `cypress`
+- pass any arguments after the image name
+
+Example: shows help
+
+```shell
+$ docker run -it -v $PWD:/e2e -w /e2e --entrypoint=cypress cypress/included:3.2.0 help
+```
+
+## Cypress open
+
+Follow [Running GUI applications using Docker for Mac](https://sourabhbajaj.com/blog/2017/02/07/gui-applications-docker-mac/) and install [XQuartz](https://www.xquartz.org) X11 server. Then see [cy-open.sh](cy-open.sh) for Docker run command that forwards XVFB events from Cypress to the X11 server running on the host machine. You should see Cypress Test Runner open and work fine.
+
+![Cypress GUI shown in X11 server](images/cy-open.png)
---
+++ cy-open.sh
@@ -0,0 +1,27 @@
+# pass DISPLAY variable pointing at the host's X11 server
+# like XQuartz
+# read https://sourabhbajaj.com/blog/2017/02/07/gui-applications-docker-mac/
+
+IP=$(ipconfig getifaddr en0)
+# this assumes that X11 server allows connections over the network
+# after we execute "/usr/X11/bin/xhost + $IP"
+DISPLAY=$IP:0
+
+# explanation of the "docker run" command line arguments
+#
+#  -it          = interactive terminal
+#  -v $PWD:/e2e = map current folder to /e2e inside the container
+#  -v /tmp/.X11-unix:/tmp/.X11-unix = map X11 socket file to communicate
+#  -w /e2e      = set working directy to /e2e
+#  -e DISPLAY   = pass environment variable DISPLAY to the container
+#  --entrypoint cypress = run "cypress" command
+#     with arguments AFTER Docker image name
+#     in our case they are "--project ." to point globally installed Cypress
+#     at the current working directory /e2e inside the container
+docker run -it \
+  -v $PWD:/e2e \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -w /e2e \
+  -e DISPLAY \
+  --entrypoint cypress \
+  cypress/included:3.2.0 open --project .
